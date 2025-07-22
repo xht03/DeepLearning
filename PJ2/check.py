8a8467c6c7a266316cae1c51663e0638f659ca89
xht03 <1620318777@qq.com>
2025-07-22 16:33:50 +0800

    7/22 16:33

--- PJ2/check.py
+++ PJ2/check.py
@@ -26,7 +26,7 @@ def check(language, gold_path, my_path):
     with open(gold_path, "r") as g_f, open(my_path, "r") as m_f:
         g_lines = g_f.readlines()
         m_lines = m_f.readlines()
-        # assert len(g_lines) == len(m_lines), "Length is Not Equal."
+        assert len(g_lines) == len(m_lines), "Length is Not Equal."
         for i in range(len(g_lines)):
             if g_lines[i] == "\n":
                 continue
@@ -34,6 +34,30 @@ def check(language, gold_path, my_path):
             m_word, m_tag = m_lines[i].strip().split(" ")
             y_true.append(g_tag)
             y_pred.append(m_tag)
+    print(metrics.classification_report(
+        y_true = y_true, y_pred=y_pred, labels=sort_labels[1:], digits=4
+    ))
+    return
+
+def check(language, gold_path, my_path):
+    if language == "English":
+        sort_labels = sorted_labels_eng
+    else:
+        sort_labels = sorted_labels_chn
+    y_true = []
+    y_pred = []
+    with open(gold_path, "r") as g_f, open(my_path, "r") as m_f:
+        g_lines = g_f.readlines()
+        m_lines = m_f.readlines()
+        assert len(g_lines) == len(m_lines), "Length is Not Equal."
+        for i in range(len(g_lines)):
+            if g_lines[i] == "\n":
+                continue
+            g_word, g_tag = g_lines[i].strip().split(" ")
+            m_word, m_tag = m_lines[i].strip().split(" ")
+            y_true.append(g_tag)
+            y_pred.append(m_tag)
+    print(f"Micro F1 Score = {metrics.f1_score(y_true, y_pred, average='micro'):.4f}")
     print(metrics.classification_report(
         y_true = y_true, y_pred=y_pred, labels=sort_labels[1:], digits=4
     ))
